a4f1ab34dfe492ddef03f500f798236c3abb2566
Dmytro Bulhakov <mail@dbulgakov.com>
2022-04-11 18:05:41 +0300

    added task 27 solution

---
+++ pointers/27.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        pos = 0
+        counter = 0
+
+        for i, v in enumerate(nums):
+            if v != val:
+                nums[i], nums[pos] = nums[pos], nums[i]
+                pos += 1
+                counter += 1
+
+        return counter
+
+
+s = Solution()
+n = [0, 1, 2, 2, 3, 0, 4, 2]
+print(s.removeElement(n, 2))
+print(n)
