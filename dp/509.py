228254b97775014d6e2d7ceda021c74756d42b0a
Dmytro Bulhakov <mail@dbulgakov.com>
2022-04-13 15:46:34 +0300

    added task 509 solution

---
+++ dp/509.py
@@ -0,0 +1,10 @@
+import functools
+
+
+class Solution:
+    @functools.lru_cache(maxsize=None)
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+        else:
+            return self.fib(n - 1) + self.fib(n - 2)
