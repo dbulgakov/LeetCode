90fe551a21fa10cc64083fa6a79cd4b2fcf7bf0f
Dmytro Bulhakov <mail@dbulgakov.com>
2022-04-20 17:36:12 +0300

    added task 153 solution

---
+++ bs/153.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if nums[0] <= nums[len(nums) - 1]:
+            return nums[0]
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            middle = left + (right - left) // 2
+
+            if nums[middle] > nums[len(nums) - 1]:
+                left = middle + 1
+            elif middle - 1 >= 0 and nums[middle - 1] > nums[middle]:
+                return nums[middle]
+            else:
+                right = middle - 1
+
+
+s = Solution()
+print(s.findMin([4, 5, 6, 7]))
