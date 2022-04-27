e7759ca592e09893590b94b9fb0be61772defe59
Dmytro Bulhakov <mail@dbulgakov.com>
2022-04-27 16:32:29 +0300

    added task 209 solution

---
+++ pointers/209.py
@@ -0,0 +1,20 @@
+import math
+from typing import List
+
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        window_sum, window_start = 0, 0
+        min_length = math.inf
+
+        for window_end in range(len(nums)):
+            window_sum += nums[window_end]
+            while window_sum >= target:
+                min_length = min(min_length, window_end - window_start + 1)
+                window_sum -= nums[window_start]
+                window_start += 1
+
+        if min_length == math.inf:
+            return 0
+
+        return min_length
