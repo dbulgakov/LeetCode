2cd52ea18b7910513edacc3fe2c7300673e84e28
Dmytro Bulhakov <mail@dbulgakov.com>
2022-05-13 13:35:40 +0300

    add task 36 solution

---
+++ arrays/36.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
+
+        for i in range(9):
+            for j in range(9):
+                val = board[i][j]
+                if val == '.':
+                    continue
+                elif (val in rows[i]) or (val in cols[j]) or (val in squares[(i // 3, j // 3)]):
+                    return False
+
+                rows[i].add(val)
+                cols[j].add(val)
+                squares[(i // 3, j // 3)].add(val)
+
+        return True
