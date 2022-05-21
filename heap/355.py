0db079dfa058ed6d0e6752c1cb817f7fd10ebf76
Dmytro Bulhakov <mail@dbulgakov.com>
2022-05-21 17:05:02 +0300

    added task 355 solution

---
+++ heap/355.py
@@ -0,0 +1,28 @@
+class Twitter:
+
+    def __init__(self):
+        self.user_folowees = defaultdict(set)
+        self.user_feeds = defaultdict(list)
+        self.tweet_count = 0
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweet_count += 1
+        if len(self.user_feeds[userId]) == 10:
+            heapq.heappushpop(self.user_feeds[userId], (self.tweet_count, tweetId))
+        else:
+            heapq.heappush(self.user_feeds[userId], (self.tweet_count, tweetId))
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        feed = []
+
+        for uid in list(self.user_folowees[userId]) + [userId]:
+            feed = heapq.nlargest(10, feed + self.user_feeds[uid], key=lambda x: x[0])
+
+        return [x[1] for x in feed]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.user_folowees[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.user_folowees[followerId]:
+            self.user_folowees[followerId].remove(followeeId)
