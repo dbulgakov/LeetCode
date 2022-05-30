6b02f0617877261cb64361a9e4574420e475cfb7
Dmytro Bulhakov <mail@dbulgakov.com>
2022-05-30 12:12:51 +0300

    updated task 981 solution

--- ht/981.py
+++ ht/981.py
@@ -1,37 +1,26 @@
+from collections import defaultdict
+
+
 class TimeMap:
 
     def __init__(self):
-        self.store = {}
+        self.store = defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        val_lst = self.store.get(key, [])
-        val_lst.append((timestamp, value))
-
-        self.store[key] = val_lst
+        self.store[key].append((value, timestamp))
 
     def get(self, key: str, timestamp: int) -> str:
-        if key not in self.store:
-            return ''
-
-        if timestamp >= self.store[key][-1][0]:
-            return self.store[key][-1][1]
+        res = ''
+        values = self.store[key]
 
         left = 0
-        right = len(self.store[key]) - 1
-        key_values = self.store[key]
+        right = len(values) - 1
 
         while left <= right:
-            middle = left + (right - left) // 2
-
-            if key_values[middle][0] == timestamp:
-                return key_values[middle][1]
-            elif key_values[middle][0] > timestamp:
-                if key_values[middle - 1][0] < timestamp:
-                    return key_values[middle - 1][1]
-                right = middle - 1
+            mid = (left + right) // 2
+            if values[mid][1] <= timestamp:
+                res = values[mid][0]
+                left = mid + 1
             else:
-                if key_values[middle + 1][0] > timestamp:
-                    return key_values[middle][1]
-                left = middle + 1
-
-        return ''
+                right = mid - 1
+        return res
