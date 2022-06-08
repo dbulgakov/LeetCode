4419d4daac440b6d390f48c8d2ee6f3b1137274f
Dmytro Bulhakov <mail@dbulgakov.com>
2022-06-08 16:02:38 +0300

    added task 684 solution

---
+++ dfs/684.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+
+class Solution:
+    def findRedundantConnection(self, edges):
+        graph = defaultdict(set)
+
+        def dfs(source, target):
+            if source not in seen:
+                seen.add(source)
+                if source == target:
+                    return True
+                return any(dfs(nei, target) for nei in graph[source])
+
+        for u, v in edges:
+            seen = set()
+            if u in graph and v in graph and dfs(u, v):
+                return u, v
+            graph[u].add(v)
+            graph[v].add(u)
