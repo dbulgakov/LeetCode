6cfd8b0bbbbe4ec68cf9298f84c431d05a68f74a
Dmytro Bulhakov <mail@dbulgakov.com>
2022-06-10 13:29:12 +0300

    added task 778 solution

---
+++ bfs/778.py
@@ -0,0 +1,29 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        if not grid:
+            return -1
+
+        q = [(grid[0][0], 0, 0)]
+
+        seen = set()
+
+        while q:
+            duration, row, col = heapq.heappop(q)
+
+            if row == len(grid) - 1 and col == len(grid) - 1:
+                return duration
+
+            for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                r = row + dr
+                c = col + dc
+
+                if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and (r, c) not in seen:
+                    new_duration = max(duration, max(grid[0][0], grid[r][c]))
+                    seen.add((r, c))
+                    heapq.heappush(q, (new_duration, r, c))
+
+        return -1
