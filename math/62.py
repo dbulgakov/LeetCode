0dd28b4fb1d21f208e7f843ff303cff137a0b535
Dmytro Bulhakov <mail@dbulgakov.com>
2022-06-14 12:30:12 +0300

    updated task 62

--- math/62.py
+++ math/62.py
@@ -1,6 +1,9 @@
-import math
-
-
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return math.comb(m + n - 2, n - 1)
+        d = [[1] * n for _ in range(m)]
+
+        for col in range(1, m):
+            for row in range(1, n):
+                d[col][row] = d[col - 1][row] + d[col][row - 1]
+
+        return d[m - 1][n - 1]
