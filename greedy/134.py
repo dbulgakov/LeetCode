b76abedd20b80b0228b0516dc3f3f4d98d761d34
Dmytro Bulhakov <mail@dbulgakov.com>
2022-06-16 13:18:59 +0300

    added task 134 solution

---
+++ greedy/134.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(cost) > sum(gas):
+            return -1
+
+        start, total = 0, 0
+
+        for i in range(len(gas)):
+            total += gas[i] - cost[i]
+
+            if total < 0:
+                total = 0
+                start = i + 1
+
+        return start
